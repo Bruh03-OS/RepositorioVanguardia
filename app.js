cffdbaf35b0a1e7c553e5b20b241e9f372f8b84c
Julio Salgado <jyahirsalgado@gmail.com>
2024-04-29 19:29:56 -0600

    Conexion exitosa con la base de datos de mongo

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+
 const app = express();
 app.use(bodyParser.json());
-
-let inventario = [
-    {
-        id: 1,
-        nombre: "Motor V8",
-        entrada: "2024-04-25",
-        salida: null,
-        precio: 10.50,
-        descripcion: "El motor V8, con sus ocho cilindros dispuestos en forma de V, es un icono de potencia y rendimiento en la industria automotriz. Su diseño compacto y eficiente ofrece una combinación perfecta entre fuerza y suavidad, mientras que su característico sonido de escape es la sinfonía de los amantes de la velocidad. Con una entrega de potencia suave y lineal, el motor V8 es la elección preferida para aquellos que buscan una experiencia de conducción emocionante y satisfactoria.",
-        estado: "Disponible"
-    }
-];
 
 /* Aqui empieza el CRUD de inventario */
 app.get('/inventario', (req, res) => {
